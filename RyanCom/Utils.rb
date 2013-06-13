236c8f93311fd4016728a6632dc6b342feac3e4c
aki017 <aki@aki017.info>
2013-06-13 22:11:38 +0900

    Refactored

--- RyanCom/Utils.rb
+++ RyanCom/Utils.rb
@@ -1,3 +1,23 @@
+class String
+  def ljust(width, padding=' ')
+    padding_size = [0, width - size].max
+    self + padding * padding_size
+  end
+  def rjust(width, padding=' ')
+    padding_size = [0, width - size].max
+    padding * padding_size + self
+  end
+  def center(width, padding=' ')
+    padding_size = [0, width - size].max
+    padding * ( padding_size / 2 ) + self + padding * ( ( padding_size + 1 ) / 2 )
+  end
+
+  def size
+    each_char.map{|c| c.bytesize == 1 ? 1 : 2}.reduce(0, &:+)
+  end
+end
+
+
 module RyanCom
   module Utils
     def command_exists?(command)
--- screenxtv-comment.rb
+++ screenxtv-comment.rb
@@ -14,25 +14,6 @@ require "RyanCom/Output"
 require "RyanCom/Utils"
 require "RyanCom/Comment"
 require "RyanCom/Notify"
-
-class String
-  def ljust(width, padding=' ')
-    padding_size = [0, width - size].max
-    self + padding * padding_size
-  end
-  def rjust(width, padding=' ')
-    padding_size = [0, width - size].max
-    padding * padding_size + self
-  end
-  def center(width, padding=' ')
-    padding_size = [0, width - size].max
-    padding * ( padding_size / 2 ) + self + padding * ( ( padding_size + 1 ) / 2 )
-  end
-
-  def size
-    each_char.map{|c| c.bytesize == 1 ? 1 : 2}.reduce(0, &:+)
-  end
-end
  
 channel = nil; 
 OptionParser.new do |opt|
